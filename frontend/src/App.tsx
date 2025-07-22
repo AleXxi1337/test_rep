bfd4cf293783cbad064bd93de1b32e4e15a557c0
Donwload <a69553891@gmail.com>
2025-07-22 16:54:44 +0300

    delete comment in App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,3 @@
 export const App = () => {
   return <></>;
 };
-
-// // src/components/BadComponent.tsx
-// import React, { useState, useEffect } from 'react';
-
-// interface Props {
-//   show: boolean;
-// }
-// iiaaaaa
-// export const App: React.FC<Props> = ({ show }) => {
-//   const [count, setCount] = useState(0);
-//   const unusedVariable = 42; // ❌ не используется — сработает правило no-unused-vars
-
-//   if (show) {
-//     useEffect(() => {
-//       console.log('shown count:', count);
-//     }, []); // ❌ Использует count, но не указан в зависимости — нарушение react-hooks/exhaustive-deps
-//   }
-
-//   return <div>{count}</div>;
-// };
