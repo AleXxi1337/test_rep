8897ee1887a9fc15939ffc9a39147bc094adfff7
Donwload <a69553891@gmail.com>
2025-07-23 18:25:28 +0300

    add back

--- backend/src/main.py
+++ backend/src/main.py
@@ -7,7 +7,12 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["*"]
+    allow_origins=["http://localhost:5173"],  # Укажите конкретные домены фронтенда
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+    max_age=600
 )
 
 app.include_router(router_v1)
--- frontend/src/api/backend.ts
+++ frontend/src/api/backend.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const handleClick = async () => {
   try {
-    const response = await axios.get('http://localhost:8000/v1/test/get_text');
+    const response = await axios.get('http://127.0.0.1:8000/v1/test/get_text');
     console.log('Данные получены:', response.data);
   } catch (err) {
     console.error('Ошибка запроса:', err);
